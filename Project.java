ffbf885d9dc40e9875677fbe7325a3970655e5e1
jHazem <junbsher@gmail.com>
2021-11-03 16:20:29 -0500

    Project Class Test

---
+++ Project.java
@@ -0,0 +1,19 @@
+public class Project {
+  private String name;
+  private String description;
+
+  public String elevatorPitch() {
+    this.getName();
+    this.getName();
+
+    return (this.getName() + " : " + this.getDescription());
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+}
---
+++ ProjectTest.java
@@ -0,0 +1,8 @@
+public class ProjectTest {
+  public static void main(String[] args) {
+    Project x1 = new Project();
+    System.out.println(x1);
+
+  }
+              
+}
