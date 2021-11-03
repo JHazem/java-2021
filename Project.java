a54e5c0ca3f5cda2e6f976c387827dcc647030c7
jHazem <junbsher@gmail.com>
2021-11-03 16:40:57 -0500

    ProjectClass

--- Project.java
+++ Project.java
@@ -1,19 +1,33 @@
 public class Project {
   private String name;
   private String description;
+  private double cost;
 
   public String elevatorPitch() {
-    this.getName();
-    this.getName();
+    return (name + "(" + cost + ")" + " : " + description);
+  }
 
-    return (this.getName() + " : " + this.getDescription());
+  public void setName(String theName) {
+    this.name = theName;
   }
 
   public String getName() {
     return name;
   }
 
+  public void setDescription(String theDescription) {
+    this.description = theDescription;
+  }
+
   public String getDescription() {
     return description;
+  }
+  
+  public void setCost(double theCost) {
+    this.cost = theCost;
+  }
+
+  public double getCost() {
+    return (cost);
   }
 }
--- ProjectTest.java
+++ ProjectTest.java
@@ -1,7 +1,7 @@
 public class ProjectTest {
   public static void main(String[] args) {
-    Project x1 = new Project();
-    System.out.println(x1);
+    // Project x1 = new Project();
+    // System.out.println(x1);
 
   }
               
