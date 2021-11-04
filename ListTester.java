c37f35f497a97547c63ee127ecf856f860b54c3a
Hazem Juned <90466434+jHazem@users.noreply.github.com>
2021-11-04 12:38:40 -0500

    Add files via upload

---
+++ ListTester.java
@@ -0,0 +1,9 @@
+public class ListTester {
+  public static void main(String[] args) {
+    SinglyLinkedList sll = new SinglyLinkedList();
+    sll.add(3);
+    sll.add(4);
+    sll.remove();
+    sll.printValues();
+  }
+}
