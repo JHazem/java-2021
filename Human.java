64643080f4acb0610622f70032039f788ca76c28
Hazem Juned <90466434+jHazem@users.noreply.github.com>
2021-11-05 09:47:48 -0500

    ProjectMaster1

---
+++ Human.java
@@ -0,0 +1,17 @@
+public class Human {
+
+  private String variousName = "";
+  private int strength = 3;
+  private int stealth = 3;
+  private int intelligence = 3;
+  private int health = 100;
+
+  public Haman(){
+    this.variousName =name;
+  }
+
+
+  public void attack(Human human) {
+
+  }
+}
---
+++ HumanTest.java
@@ -0,0 +1,8 @@
+public class HumanTest {
+ public static void main(String[] args) {
+   Human  human1 = new Human("Sam");
+   Human  wizard1 = new Human("Dan");
+   Human ninja1 = new Human("Van");
+   
+ } 
+}
