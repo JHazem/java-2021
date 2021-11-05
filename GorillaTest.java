58e0122e3ad1fd66ce5834a4f779c998b9fd6aba
Hazem Juned <90466434+jHazem@users.noreply.github.com>
2021-11-04 21:54:05 -0500

    ZooKeeper1

---
+++ GorillaTest.java
@@ -0,0 +1,10 @@
+public class GorillaTest {
+   public static void main(String[] args) {
+     Gorilla zooGorilla = new Gorilla();
+     zooGorilla.eatBananas();
+     zooGorilla.eatBananas();
+     zooGorilla.displayEnergy();
+     zooGorilla.climb();
+     zooGorilla.displayEnergy();
+   }           
+}
