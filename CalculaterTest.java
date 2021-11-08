034c50216cab585984b3f5d27818fa82271794f1
Hazem Juned <90466434+jHazem@users.noreply.github.com>
2021-11-08 15:40:07 -0600

    Add files via upload

---
+++ CalculaterTest.java
@@ -0,0 +1,10 @@
+package com.codingdojo.java.calculator1;
+
+public class CalculatorTest {
+
+  public static void main(String[] args) {
+		Calculator testCalc1 = new Calculator(10.5,"+",5.2);
+		testCalc1.performOperation();
+		testCalc1.getResults();
+		
+	}
