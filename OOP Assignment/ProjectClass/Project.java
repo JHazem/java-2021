1fe76ac70234bd38ab202f4f1adbf79a08957327
Hazem Juned <90466434+jHazem@users.noreply.github.com>
2021-11-09 12:46:56 -0600

    Add files via upload

--- OOP Assignment/ProjectClass/Project.java
+++ OOP Assignment/ProjectClass/Project.java
@@ -1,1 +1,34 @@
+package ProjectClass;
+public class Project {
+  private String name;
+  private String description;
+  private double cost;
 
+  public String elevatorPitch() {
+    return (name + "(" + cost + ")" + " : " + description);
+  }
+
+  public void setName(String theName) {
+    this.name = theName;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setDescription(String theDescription) {
+    this.description = theDescription;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+  
+  public void setCost(double theCost) {
+    this.cost = theCost;
+  }
+
+  public double getCost() {
+    return (cost);
+  }
+}
---
+++ OOP Assignment/ProjectClass/ProjectTest.java
@@ -0,0 +1,9 @@
+package ProjectClass;
+public class ProjectTest {
+  public static void main(String[] args) {
+    // Project x1 = new Project();
+    // System.out.println(x1);
+
+  }
+              
+}
