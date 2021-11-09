8ad1a77ce64662b02852e4140a4c8c21126fff9e
Hazem Juned <90466434+jHazem@users.noreply.github.com>
2021-11-09 12:29:22 -0600

    Add files via upload

--- java-Funtemental/ArrayList/ArrayListTest.java
+++ java-Funtemental/ArrayList/ArrayListTest.java
@@ -1,1 +1,12 @@
+import java.util.ArrayList;
+public class ArrayListTest {
+       public ArrayList<String> testing(){
+        ArrayList<String> myList = new ArrayList<>();
+        myList.add("13");
+        myList.add("hello world");
+        myList.add(48);
+        myList.add("Goodbye World");
+        return myList;
+    }
+}    
 
