4959be81478e7be015dbc34f288af10ea0fb814b
Hazem Juned <90466434+jHazem@users.noreply.github.com>
2021-11-09 12:50:22 -0600

    Delete CalculaterTest.java

--- CalculaterTest.java
+++
@@ -1,9 +0,0 @@
-
-public class CalculatorTest {
-
-  public static void main(String[] args) {
-		Calculator testCalc1 = new Calculator(10.5,"+",5.2);
-		testCalc1.performOperation();
-		testCalc1.getResults();
-		
-	}
