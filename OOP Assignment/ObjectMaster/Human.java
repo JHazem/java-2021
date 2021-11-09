aca40925474fe2628e98b687aa4ecad76056fb7d
Hazem Juned <90466434+jHazem@users.noreply.github.com>
2021-11-09 12:44:18 -0600

    Add files via upload

--- OOP Assignment/ObjectMaster/Human.java
+++ OOP Assignment/ObjectMaster/Human.java
@@ -1,1 +1,59 @@
+public class Human {
 
+  private String variousName = "";
+  private int strength = 3;
+  private int stealth = 3;
+  private int intelligence = 3;
+  private int health = 100;
+
+  public Haman(){
+    this.variousName =name;
+  }
+
+  public void setName(String name) {
+    this.variousName = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+  
+  public void setStealth(int stealth) {
+    this.stealth = stealth;
+  }
+
+  public int getStealth() {
+    return stealth;
+  }
+  
+  public void setStrength(int strength) {
+    this.strength = strength;
+  }
+
+  public int getStrength() {
+    return strength;
+  }
+  
+  
+  public void setIntelligence(int intelligence) {
+    this.intelligence = intelligence;
+  }
+
+  public int getIntelligence() {
+    return intelligence;
+  }
+  
+  
+  public void setHealth(int health) {
+    this.health = health;
+  }
+
+  public int getHealth() {
+    return health;
+  }
+  
+
+  public void attack(Human human) {
+
+  }
+}
