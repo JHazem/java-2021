97b1cbc0432d94d263819449c6c2163953415ccd
Hazem Juned <90466434+jHazem@users.noreply.github.com>
2021-11-09 12:51:19 -0600

    Delete ListTester.java

--- ListTester.java
+++
@@ -1,9 +0,0 @@
-public class ListTester {
-  public static void main(String[] args) {
-    SinglyLinkedList sll = new SinglyLinkedList();
-    sll.add(3);
-    sll.add(4);
-    sll.remove();
-    sll.printValues();
-  }
-}
