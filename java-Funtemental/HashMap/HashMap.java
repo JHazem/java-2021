4f21f9a21c5bbab000f163a0b3c37bf5565cc37d
Hazem Juned <90466434+jHazem@users.noreply.github.com>
2021-11-09 12:32:49 -0600

    Add files via upload

--- java-Funtemental/HashMap/HashMap.java
+++ java-Funtemental/HashMap/HashMap.java
@@ -1,1 +1,12 @@
+import java.util.HashMap;
 
+public class TrackList {
+  public void trackList() {
+    HashMap<String, String> tracks = new HashMap<>();
+    tracks.put("You right", "HHHHH");
+    tracks.put("Save your Tears", "MMMMMM");
+    tracks.put("Triator", " NNNNNN");
+    tracks.put("My Universe", "OOOOOO");
+    System.out.println( tracks.get("Triator"));
+  }
+}
