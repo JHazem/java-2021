ac0086e167539b354266d9182305f62aa986edcc
Hazem Juned <90466434+jHazem@users.noreply.github.com>
2021-11-09 22:18:07 -0600

    Add files via upload

---
+++ Java Spring/Strings/StringsApplication.java
@@ -0,0 +1,24 @@
+package com.hazem.strings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class StringsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringsApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String hello() {
+		return "Hello client! How are you doing?";
+	}
+	@RequestMapping("/random")
+	public String Well() {
+		return "It was a pleasure to meeting you today, and I hope to do the best for you!";
+	}
+}
