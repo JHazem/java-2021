284a897e1843f295e07769584ce2c08f35f9ca16
Hazem Juned <90466434+jHazem@users.noreply.github.com>
2021-11-10 23:27:29 -0600

    Add files via upload

---
+++ Java Spring/Routing/DojoControllerApplication.java
@@ -0,0 +1,33 @@
+package com.hazem.codingcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+
+public class DojoControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DojoControllerApplication.class, args);
+	}
+	
+	@RequestMapping("/dojo")
+	public String dojo (@PathVariable String dojo) {
+		return "The dojo is awesome!";
+	}
+	
+	@RequestMapping("/dojo/burbank")
+	public String burbank (@PathVariable String dojo, @PathVariable String burbank) {
+		return "Burbank Dojo is located in Southern California";
+	}
+	
+	@RequestMapping("/dojo/san-jose")
+	public String san (@PathVariable String dojo, @PathVariable String sanjose) {
+		return "SJ dojo is the headquarters";
+	}
+	
+}
