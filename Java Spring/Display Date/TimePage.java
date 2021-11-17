5d330ff326158e4fe617e4e0cc33d63bb2aeaf90
Hazem Juned <90466434+jHazem@users.noreply.github.com>
2021-11-17 08:56:49 -0600

    Update TimePage.java

--- Java Spring/Display Date/TimePage.java
+++ Java Spring/Display Date/TimePage.java
@@ -1,5 +1,20 @@
 package com.example.date.controllers;
 
-public class TimePage {
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TimeCtrl {
+	@RequestMapping("/time")
+	public String time(Model model) {
+		String timePattern = "hh:mm ";
+		SimpleDateFormat timeFormat = new SimpleDateFormat(timePattern);
+		String date = timeFormat.format(new Date());	
+		model.addAttribute("date", date);
+		return "time.jsp";
+  }
 }
