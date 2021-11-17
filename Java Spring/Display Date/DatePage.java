032ee29f0f151316615d1685c71bddf700c696b9
Hazem Juned <90466434+jHazem@users.noreply.github.com>
2021-11-17 08:54:57 -0600

    Update DatePage.java

--- Java Spring/Display Date/DatePage.java
+++ Java Spring/Display Date/DatePage.java
@@ -1,5 +1,14 @@
 package com.example.date.controllers;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-public class DatePage {
-
+@Controller
+public class DateCtrl {
+	@RequestMapping("/date")
+	public String date(Model model) {
+    		String datePattern = "dd, MMMMM, yyyy";
+		    SimpleDateFormat dateFormat = new SimpleDateFormat(datePattern);
+		    String date = dateFormat.format(new Date());	
+		    model.addAttribute("date", date);
+	    	return "date.jsp";
 }
